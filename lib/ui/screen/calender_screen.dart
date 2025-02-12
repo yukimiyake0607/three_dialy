cb98ab906bba3a832a8a02824c6b4ff7672a8976
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-12 18:09:36 +0900

    add: ボトムナビゲーションで設定するWidgetを仮で追加
    - 全てStatelessWidgetとしている

---
+++ lib/ui/screen/calender_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalenderScreen extends StatelessWidget {
+  const CalenderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/ui/screen/diary_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiaryScreen extends StatelessWidget {
+  const DiaryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
