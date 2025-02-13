71f6acd1282f483f0d651bb402e357eb1cde9623
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-13 11:45:49 +0900

    add: TableCalenderを追加（基本的な実装のみ）

--- lib/ui/screen/calender/calender_screen.dart
+++ lib/ui/screen/calender/calender_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalenderScreen extends StatelessWidget {
+class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
 
   @override
+  State<CalenderScreen> createState() => _CalenderScreenState();
+}
+
+class _CalenderScreenState extends State<CalenderScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('カレンダー'),
+    return TableCalendar(
+      focusedDay: _focusedDay,
+      firstDay: DateTime.utc(2025, 1, 1),
+      lastDay: DateTime.utc(2099, 12, 31),
+
+      // 日付が選択された時の処理
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+          _focusedDay = focusedDay;
+        });
+      },
+
+      // 選択された日付に色を塗るか判定
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+
+      calendarStyle: const CalendarStyle(
+        selectedDecoration: BoxDecoration(
+          color: Colors.green,
+          shape: BoxShape.circle,
+        ),
+      ),
     );
   }
 }
