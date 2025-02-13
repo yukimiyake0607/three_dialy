018396f077353ab6f8b67772cf584fb67e9ec086
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-13 18:40:46 +0900

    fix: ビルドできない問題を修正
    - GoogleService-Infoが2つあったから？
    - 一方を削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mindow/app/app.dart';
+import 'package:mindow/firebase_options.dart';
 
-void main() {
-  initializeDateFormatting('ja_JP', null).then((_) => runApp(const MainApp()));
+void main() async {
+  // Flutter EngineとWidgetsの初期化
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebaseの初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // 日本語の日付フォーマットの初期化
+  await initializeDateFormatting('ja_JP', null);
+
+  runApp(const MainApp());
 }
