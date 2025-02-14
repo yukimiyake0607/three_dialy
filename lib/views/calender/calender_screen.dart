41a51bee9d3394553a811e006b06c986e286b38f
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-14 22:12:02 +0900

    refactor: TableCalenderを切り出し

--- lib/views/calender/calender_screen.dart
+++ lib/views/calender/calender_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:mindow/views/calender/widgets/table_calender.dart';
 
 class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
@@ -9,52 +9,13 @@ class CalenderScreen extends StatefulWidget {
 }
 
 class _CalenderScreenState extends State<CalenderScreen> {
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      focusedDay: _focusedDay,
-      firstDay: DateTime.utc(2025, 1, 1),
-      lastDay: DateTime.utc(2099, 12, 31),
-
-      // 日付が選択された時の処理
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selectedDay = selectedDay;
-          _focusedDay = focusedDay;
-        });
-      },
-
-      // 選択された日付に色を塗るか判定
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-
-      onPageChanged: (focusedDay) {
-        _focusedDay = focusedDay;
-      },
-
-      calendarStyle: const CalendarStyle(
-        selectedDecoration: BoxDecoration(
-          color: Colors.green,
-          shape: BoxShape.circle,
-        ),
-        weekendTextStyle: TextStyle(color: Colors.red),
-      ),
-
-      // 曜日を日本語表記
-      daysOfWeekStyle: const DaysOfWeekStyle(
-        weekendStyle: TextStyle(color: Colors.red),
-      ),
-
-      headerStyle: const HeaderStyle(
-        formatButtonVisible: false,
-        titleCentered: true, // タイトルを中央寄せ
-      ),
-
-      locale: 'ja_JP',
+    return const Column(
+      children: [
+        DiaryCalender(),
+        Text('ここに表示'),
+      ],
     );
   }
 }
