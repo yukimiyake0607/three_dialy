e6765c26e2b3abce584f4eb9911cb2ca6e0d8337
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-17 16:41:21 +0900

    update: TableCalenderの状態管理をriverpodに変更

--- lib/views/calender/widgets/table_calender.dart
+++ lib/views/calender/widgets/table_calender.dart
@@ -1,40 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class DiaryCalender extends StatefulWidget {
+// focusedDay
+final focusedDayProvider =
+    StateNotifierProvider<FocusedDayNotifier, DateTime>((ref) {
+  return FocusedDayNotifier();
+});
+
+class FocusedDayNotifier extends StateNotifier<DateTime> {
+  FocusedDayNotifier([DateTime? initialDate])
+      : super(initialDate?.copyWith(
+                hour: 0,
+                minute: 0,
+                second: 0,
+                millisecond: 0,
+                microsecond: 0) ??
+            DateTime.now().copyWith(
+                hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0));
+
+  void updateFocusedDay(DateTime? focusedDay) {
+    if (focusedDay == null) return;
+    state = focusedDay.copyWith(
+        hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
+  }
+}
+
+// selectedDay
+final selectedDayProvider =
+    StateNotifierProvider<SelectedDayNotifier, DateTime?>((ref) {
+  return SelectedDayNotifier();
+});
+
+class SelectedDayNotifier extends StateNotifier<DateTime?> {
+  SelectedDayNotifier() : super(null);
+
+  void onDaySelected(DateTime? selectedDay) {
+    if (selectedDay == null) return;
+    state = selectedDay.copyWith(
+        hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
+  }
+
+  void clearSelection() => state = null;
+}
+
+class DiaryCalender extends ConsumerWidget {
   const DiaryCalender({super.key});
 
   @override
-  State<DiaryCalender> createState() => _DiaryCalenderState();
-}
-
-class _DiaryCalenderState extends State<DiaryCalender> {
-  DateTime _focusedDay = DateTime.now();
-
-  DateTime? _selectedDay;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final calenderFocusedDay = ref.watch(focusedDayProvider);
+    final calenderSelectedDay = ref.watch(selectedDayProvider);
     return TableCalendar(
-      focusedDay: _focusedDay,
+      focusedDay: calenderFocusedDay,
       firstDay: DateTime.utc(2025, 1, 1),
       lastDay: DateTime.utc(2099, 12, 31),
 
       // 日付が選択された時の処理
       onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selectedDay = selectedDay;
-          _focusedDay = focusedDay;
-        });
+        ref.read(selectedDayProvider.notifier).onDaySelected(selectedDay);
+        ref.read(focusedDayProvider.notifier).updateFocusedDay(focusedDay);
       },
 
       // 選択された日付に色を塗るか判定
       selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
+        return calenderSelectedDay != null &&
+            isSameDay(calenderSelectedDay, day);
       },
 
       onPageChanged: (focusedDay) {
-        _focusedDay = focusedDay;
+        // StateNotifierのメソッドを使用して更新
+        ref.read(focusedDayProvider.notifier).updateFocusedDay(focusedDay);
       },
 
       calendarStyle: const CalendarStyle(
